ad63f18b933d57c944a6224b014e143afe3a0e76
rickylin1 <pandanation33@gmail.com>
2024-06-29 16:18:21 -0700

    Update Links

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,8 +4,7 @@ import PdfViewer from '../components/PdfViewer.tsx'; // Replace with your actual
 import '../styles/AboutPage.css';
 
 const AboutPage = () => {
-  // Replace 'pdfUrl' with the URL of your actual PDF file
-  const pdfUrl = 'https://drive.google.com/uc?id=1jSEKI4bAF3SHEjSUu_Ekhj7YFOOD2A2c';
+  const pdfUrl = 'https://drive.google.com/uc?id=1ISU2fiADBW3KbMww6zKo2-CACdHNx32X';
   
 
   return (
@@ -18,7 +17,7 @@ const AboutPage = () => {
         {/* Your main content sections */}
 
         <section className="pdf-viewer-section">
-          <a href = "https://personalportfolio-six-theta.vercel.app/">
+          <a href = "https://www.rickylin.us/">
             <h2>Go Back Home</h2>
           </a>
           <PdfViewer pdfUrl={pdfUrl} />
