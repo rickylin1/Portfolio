5c668c8533d80b861207f8e47aad3cbcf4789da4
rickylin1 <pandanation33@gmail.com>
2024-08-07 13:48:34 -0700

    Resume Update

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,7 +6,7 @@ import Header from '../components/Header.tsx';
 import '../styles/AboutPage.css';
 
 const AboutPage = () => {
-  const pdfUrl = 'https://drive.google.com/uc?id=1RmY7qGWzm8DL97lZ3OqaNl8MZ9eNP8Vt';
+  const pdfUrl = 'https://drive.google.com/uc?id=1cg3aINMTdvyKj7NYL5EgfshuQf7p6RYb';
   
 
   return (
